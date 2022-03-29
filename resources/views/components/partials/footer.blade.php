f267bf43a4fc26de8cf6f1c0304fc82efc0fbc7a
Clemente Nogueira <git.3gear@gmail.com>
2022-03-29 22:39:28 +0100

    Landing Page styles were created.

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -1,7 +1,38 @@
-<footer>
-    <div>
+<footer class="bg-white border-t border-gray-400 shadow">
+    <div class="container max-w-4xl mx-auto flex py-8">
+
+        <div class="w-full mx-auto flex flex-wrap">
+            <div class="flex w-full md:w-1/2 ">
+                <div class="px-8">
+                    <h3 class="font-bold text-gray-900">About</h3>
+                    <p class="py-4 text-gray-600 text-sm">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex w-full md:w-1/2">
+                <div class="px-8">
+                    <h3 class="font-bold text-gray-900">Social</h3>
+                    <ul class="list-reset items-center text-sm pt-3">
+                        <li>
+                            <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Add social link</a>
+                        </li>
+                        <li>
+                            <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Add social link</a>
+                        </li>
+                        <li>
+                            <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="#">Add social link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-gray-700 text-center p-6" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2021 -  <script>document.write(new Date().getFullYear());</script>
-    Copyright:
-    <a class="text-gray-800" href="https://3gear.net/">{{ config('app.name') }}</a>
+        Copyright:
+        <a class="text-gray-800" href="https://3gear.net/">{{ config('app.name') }}</a>
     </div>
 </footer>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,6 +14,7 @@
 <x-partials.footer/>
 
 <!-- Scripts -->
+<script src="{{ asset('js/main.js') }}" defer></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
 
 <!-- Get Livewire Scripts -->
